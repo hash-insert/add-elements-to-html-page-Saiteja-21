aada286bb497af35fb1441eaba75d08d47791b65
Saiteja-21 <saitejakatkam123@gmail.com>
2023-03-07 10:36:03 +0530

    added

---
+++ index.js
@@ -0,0 +1,27 @@
+// function createEle(id,ele,text){
+//     const elepos=document.getElementById(id);
+//     const htmlEle=document.createElement(ele);
+//     const e=createTextNode(text);
+//     elepos.appendChild(htmlEle);
+
+// }
+// createEle(dom,"button","hi this is written");
+// const elepos=document.getElementById("domele");
+// const htmlEle=document.createElement("p");
+// const e=document.createTextNode("hi");
+
+// elepos.appendChild(htmlEle);
+
+// const para = document.createElement("p");
+// const node = document.createTextNode("This is new.");
+// para.appendChild(node);
+
+// const element = document.getElementById("div1");
+// const child = document.getElementById("p1");
+// element.insertBefore(para,child);
+const elepos=document.getElementById("domele");
+const htmlEle=document.createElement("p");
+const s=document.createTextNode("hiii");
+elepos.appendChild(s);
+
+
